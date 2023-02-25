da6c85fe3bc7233ca731e7512bf42ee9cd3b4982
Jollly_Rodger <andrey.teryannikov@gmail.com>
2023-02-25 16:30:45 +0200

    Task create

---
+++ src/main/java/org/example/Task1.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String str = "A man, a plan, a canal: Panama";
+    }
+}
---
+++ src/main/java/org/example/Task2.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        int[] array = {1, 4, 5, 4, 3, 2, 1, 7, 6, 2, 3, 8, 6, 1, 9};
+
+    }
+
+
+}
