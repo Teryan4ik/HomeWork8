f32e1dad18899e95573c84f06a8a42edb064c2d1
Jollly_Rodger <andrey.teryannikov@gmail.com>
2023-02-26 17:27:42 +0200

    complete tasks

--- src/main/java/org/example/Task1.java
+++ src/main/java/org/example/Task1.java
@@ -3,5 +3,23 @@ package org.example;
 public class Task1 {
     public static void main(String[] args) {
         String str = "A man, a plan, a canal: Panama";
+
+        int left = 0;
+        int right = str.length() - 1;
+        boolean cheak = true;
+
+        while (left < right) {
+            if (Character.isAlphabetic(str.charAt(left))) {
+                if (Character.isAlphabetic(str.charAt(right))) {
+                    if (str.toLowerCase().charAt(left) != str.toLowerCase().charAt(right)) {
+                        cheak = false;
+                        break;
+                    }
+                    left++;
+                }
+                right--;
+            } else left++;
+        }
+        System.out.println("palindrome " + cheak);
     }
 }
--- src/main/java/org/example/Task2.java
+++ src/main/java/org/example/Task2.java
@@ -1,11 +1,24 @@
 package org.example;
 
+import java.util.HashSet;
+
 public class Task2 {
     public static void main(String[] args) {
 
-        int[] array = {1, 4, 5, 4, 3, 2, 1, 7, 6, 2, 3, 8, 6, 1, 9};
+        int[] array = {1, 4, 5, 3, 2, 1, 7, 6, 2, 3, 8, 6, 1, 9};
+        HashSet<Integer> nums = new HashSet<>();
+        boolean cheak = true;
 
+        while (cheak) {
+            for (int n : array) {
+                if (nums.contains(n)) {
+                    System.out.println(nums);   // delete
+                    System.out.println("contains " + n);    // delete
+                    cheak = false;
+                    break;
+                } else nums.add(n);
+            }
+        }
+        System.out.println(cheak);
     }
-
-
 }
